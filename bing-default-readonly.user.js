ed76e0ab17890a42cda6e55d95ae56f48c53a940
Felix E <info@feli.ga>
2022-08-02 19:29:32 +0200

    Fixed replacement

--- bing-default-readonly.user.js
+++ bing-default-readonly.user.js
@@ -4,6 +4,7 @@
 // @homepage     https://github.com/mindsolve/bing-default-readonly
 // @source       https://github.com/mindsolve/bing-default-readonly
 // @supportURL   https://github.com/mindsolve/bing-default-readonly/issues
+// @updateURL    https://raw.githubusercontent.com/mindsolve/bing-default-readonly/main/bing-default-readonly.user.js
 // @downloadURL  https://raw.githubusercontent.com/mindsolve/bing-default-readonly/main/bing-default-readonly.user.js
 // @version      0.2
 // @description  Replaces links to documents in Bing for Work results with their read-only counterpart.
@@ -16,16 +17,18 @@
     'use strict';
     console.log("[bing-readonly] loaded.");
 
+    const actionRegex = /action=(default|edit)/i;
+
     var observer = new MutationObserver(function (mutations) {
         for (let mut of mutations) {
 
             for(let node of mut.addedNodes) {
                 // we track only elements, skip other nodes (e.g. text nodes)
-                if (!(node.innerHTML.includes("action=default"))) continue;
+                if (!(actionRegex.test(node.innerHTML))) continue;
 
                 // replace the default viewing action with "view" in all search results
                 for (const link of node.getElementsByClassName("ms-Link")) {
-                    link.href = link.href.replace("action=default", "action=View");
+                    link.href = link.href.replace(actionRegex, "action=View");
                 }
 
             }
